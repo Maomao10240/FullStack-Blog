d2fe6c3908f7bd31afd87d2680f2c6a50694fb2c
Maomao10240 <63522875+Maomao10240@users.noreply.github.com>
2023-09-22 22:17:07 -0700

    Add files via upload

---
+++ middlewares/globalHandler.js
@@ -0,0 +1,15 @@
+const globalErrHandler = (err, req, res, next) => {
+  //status
+  //message
+  //stack
+  const stack = err.stack;
+  const message = err.message;
+  const status = err.status ? err.status : "failedd";
+  const statusCode = err.statusCode ? err.statusCode : 500;
+  res.status(statusCode).json({
+    message,
+    status,
+    stack,
+  });
+};
+module.exports = globalErrHandler;
